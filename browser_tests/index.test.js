89fd1ec62d81fa79f269612ce8d203d9fa852943
Michal Tusnio <m.tusnio@gmail.com>
2024-08-24 19:38:06 +0100

    Expand browser tests

--- browser_tests/index.test.js
+++ browser_tests/index.test.js
@@ -30,22 +30,65 @@ afterEach(async () => {
     workerTarget = null
 });
 
+async function getExtensionStatus() {
+    const storage = await worker.evaluate(async () => {
+        return await chrome.storage.local.get(["enabled"])
+    })
+    const badgeData = await worker.evaluate(async () => {
+        return {
+            "text": await chrome.action.getBadgeText({}),
+            "color": await chrome.action.getBadgeBackgroundColor({}),
+        }
+    })
 
-test("clicking the extension button activates the extension", async () => {
-    const someOtherPage = await browser.newPage();
-    await someOtherPage.goto("https://www.google.com/", { waitUntil: ['domcontentloaded', "networkidle2"] });
-    await someOtherPage.bringToFront();
+    return {
+        storage,
+        badgeData
+    }
+}
 
+async function toggleExtension() {
     await worker.evaluate(async () => {
         const tabs = await chrome.tabs.query({ active: true })
         await chrome.action.onClicked.dispatch(tabs[0]);
     });
+}
+test("extension is disabled by default", async () => {
+    const someOtherPage = await browser.newPage();
+    await someOtherPage.goto("https://www.google.com/", { waitUntil: ['domcontentloaded', "networkidle2"] });
+    await someOtherPage.bringToFront();
 
-
-    const storage = await worker.evaluate(async () => {
-        return await chrome.storage.local.get(["enabled"])
-
+    const status = await getExtensionStatus()
+    expect(status.storage).toEqual({})
+    expect(status.badgeData).toEqual({
+        "text": "", "color": [0, 0, 0, 0]
     })
+})
 
-    expect(storage).toEqual({ "enabled": true })
+test("toggling the extension on/off two times works", async () => {
+    const someOtherPage = await browser.newPage();
+    await someOtherPage.goto("https://www.google.com/", { waitUntil: ['domcontentloaded', "networkidle2"] });
+    await someOtherPage.bringToFront();
+
+    for (let i = 0; i < 2; i++) {
+        // On
+        await toggleExtension()
+        // Need a slight wait for the extension to trigger
+        await new Promise((r) => setTimeout(r, 200));
+        let status = await getExtensionStatus()
+        expect(status.storage).toEqual({ "enabled": true })
+        expect(status.badgeData).toEqual({
+            "text": "On", "color": [255, 0, 0, 255]
+        })
+
+        // Off
+        await toggleExtension()
+        await new Promise((r) => setTimeout(r, 200));
+        status = await getExtensionStatus()
+        expect(status.storage).toEqual({ "enabled": false })
+        expect(status.badgeData).toEqual({
+            "text": "", "color": [0, 0, 0, 0]
+        })
+    }
+
 });

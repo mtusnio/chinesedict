4a1e6534ca59af2639f1b21e5f77a84fd70fce61
Michal Tusnio <m.tusnio@gmail.com>
2024-08-25 15:47:13 +0100

    Stub option tests

---
+++ browser_tests/options.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import * as utils from "./utils";
+let browser = null;
+let worker = null
+
+beforeEach(async () => {
+    const setupData = await utils.setupBrowser()
+    browser = setupData.browser
+    worker = setupData.worker
+});
+
+afterEach(async () => {
+    await browser.close();
+    browser = null;
+    worker = null
+});
+
+describe("the options page", function () {
+    it("loads properly", async () => {
+        const page = await browser.newPage();
+        await page.goto("chrome-extension://acjkjmnpaljkemhfmmoddfpblmigpmji/options.html");
+    })
+})

dcacccc0cf0b7186e6f6957e36753ae7b9c85396
Michal Tusnio <m.tusnio@gmail.com>
2024-08-26 00:26:29 +0100

    Add custom popup config test

--- browser_tests/popup.test.js
+++ browser_tests/popup.test.js
@@ -95,3 +95,46 @@ test("prints out a valid HTML when hovering over 有 in an HTML-rich site", asyn
 
     expect(windowHTML).toEqual("<span class=\"w-hanzi-small\">有</span>&nbsp;<span class=\"w-pinyin-small tone3\">yǒu</span>&nbsp;&nbsp;&nbsp;<span class=\"w-pinyin-small\">jau5</span><br><span class=\"w-def-small\">to have/there is/there are/to exist/to be</span><br><span class=\"w-hanzi-small\">有</span>&nbsp;<span class=\"w-pinyin-small tone3\">yǒu</span>&nbsp;&nbsp;&nbsp;<span class=\"w-pinyin-small\">jau5</span><span style=\"float: right\" class=\"w-pinyin-small\">Cant.</span><br><span class=\"w-def-small\">has or have</span><br><span class=\"w-hanzi-small\">有</span>&nbsp;<span class=\"w-pinyin-small tone3\">yǒu</span>&nbsp;&nbsp;&nbsp;<span class=\"w-pinyin-small\">jau5</span><span style=\"float: right\" class=\"w-pinyin-small\">Cant.</span><br><span class=\"w-def-small\">to have/there is/there are/to exist/to be/being/a surname/to possess/to own/used in courteous phrases expressing causing trouble/to be betrothed/to be married/to be pregnant/many/to be rich/to have money/abundant/wealthy</span><br><span class=\"w-hanzi-small\">有</span>&nbsp;<span class=\"w-pinyin-small tone3\">yǒu</span>&nbsp;&nbsp;&nbsp;<span class=\"w-pinyin-small\">jau6</span><span style=\"float: right\" class=\"w-pinyin-small\">Cant.</span><br><span class=\"w-def-small\">also/again</span><br>")
 })
+
+test("prints out a different HTML when hovering over 有 in an HTML-rich site with a custom config set up", async () => {
+    const page = await browser.newPage();
+    await page.goto(`file://${path.resolve()}/browser_tests/testdata/wiki-you.html`, { waitUntil: ['domcontentloaded', "networkidle2"] });
+    await page.bringToFront();
+
+    await worker.evaluate(async () => {
+        await chrome.storage.local.set(
+            {
+                "popupColor": "blue",
+                "fontSize": "large",
+                "simpTrad": "auto",
+                "zhuyin": "yes",
+                "grammar": "no",
+                "pinyinEnabled": "no",
+                "toneColors": "yes",
+                "toneColorScheme": "pleco",
+                "jyutpingEnabled": "no",
+                "cantoneseEntriesEnabled": "no",
+                "skritterTLD": "cn",
+                "saveToWordList": "firstEntryOnly",
+                "ttsEnabled": "yes",
+            })
+    })
+
+    await utils.toggleExtension(worker)
+    await utils.wait(500)
+
+    await page.setViewport({ width: 1280, height: 720 });
+    await utils.wait(500)
+
+    const targetSelector = 'li.spaced ::-p-text(今天) em'
+    await page.waitForSelector(targetSelector, { timeout: 6000 })
+    await page.locator(targetSelector).hover();
+    const exists = !! await page.waitForSelector(zhongwenWindowSelector, { timeout: 6000 });
+    expect(exists).toBe(true)
+
+    const windowHTML = await page.$eval(zhongwenWindowSelector, (element) => {
+        return element.innerHTML
+    })
+
+    expect(windowHTML).toEqual("<span class=\"w-hanzi\">有</span>&nbsp;<br><span class=\"tone3 w-zhuyin\">ㄧㄡˇ</span><br><span class=\"w-def\">to have/there is/there are/to exist/to be</span><br>")
+})

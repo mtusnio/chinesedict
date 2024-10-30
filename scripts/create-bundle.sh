88da8249e5cb13b0d6998007d16900d0388b0043
Michal Tusnio <m.tusnio@gmail.com>
2024-10-30 20:36:25 +0000

    Add create bundle for firefox

--- scripts/create-bundle.sh
+++ scripts/create-bundle.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-rm ./chinese-dict.zip || true
+if [[ -z $1 ]]; then
+    echo "Needs one of the following parameters: chrome firefox"
+    exit 1
+fi
+TYPE=$1
+FILENAME=chinese-dict-$TYPE.zip
+
+rm ./$FILENAME || true
 echo "Zipping all files apart from manifest.json"
 find . | grep -P '(\.png|\.html|\.css|(^(?!.*\.test\.js)).*\.js$|\./data)' | grep -v -P '(node_modules|browser_tests|mock-extension-apis.js|eslint)' | zip -@ chinese-dict.zip
 EXT_PATH=$(pwd)
@@ -16,9 +23,16 @@ cd /tmp/chinese-dict
 echo "Generating manifest.json"
 
 mkfifo manifest.json
-cat $EXT_PATH/manifest.json | egrep -v '"key"' > /tmp/chinese-dict/manifest.json &
+cat $EXT_PATH/manifest.json | egrep -v '"key"' > ./manifest-file.json
+
+if [[ $TYPE == "firefox " ]]; then
+    echo "Performing firefox substitution"
+    sed -i 's/"service_worker": "worker.js"/"scripts": [ "worker.js" ]/g' ./manifest-file.json
+fi
+
+cat ./manifest-file.json > ./manifest.json &
 
 echo "Zipping manifest.json fifo"
 
-zip --fifo $EXT_PATH/chinese-dict.zip manifest.json
+zip --fifo $EXT_PATH/$FILENAME manifest.json
 rm -rf /tmp/chinese-dict
